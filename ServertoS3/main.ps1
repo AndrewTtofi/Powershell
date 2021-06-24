4d791c1546b7fa4dca65e84f854ee1d7624b8df8
AndrewTtofi <andreas.ttofi>
2021-06-24 14:23:34 +0300

    script update

--- ServertoS3/main.ps1
+++ ServertoS3/main.ps1
@@ -1,3 +1,12 @@
-$testvar = (get-date) - (get-date "10/22/2020 12:20:1 ")
 
+$d = [datetime](Get-ItemProperty -Path C:\Users\andreas.t\Test -Name LastWriteTime).lastwritetime
+$testvar = (get-date) - $d
 $testvar.Days
+if (($testvar.Days -gt 0 ) -eq $true){
+    Move-Item -Path C:\Users\andreas.t\Test\thisisatest.txt -Destination C:\Users\andreas.t\Test2
+    Write-Output "file transferred"
+}
+
+
+#$writedate = Get-ItemProperty -Path $source -Name LastWriteTime
+#Move-Item -Path C:\test.txt -Destination E:\Temp\tst.txt
